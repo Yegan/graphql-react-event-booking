a3d2023854618ca1c9e928824cab85505fb6556e
yegan <yeganj@gmail.com>
2019-11-12 15:00:33 +0200

    graphql setup

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const graphqlHttp = require('express-graphql');
 const app = express();
+const { buildSchema } = require('graphql');
+
 
 app.use(bodyParser.json());
 
+app.use('/graphql', graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        
+        type RootMutation {
+            createEvent(name: String): String
+        }
 
-app.get('/',(req,res,next)=>{
-    res.send("Hello world!");
-})
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+            events: ()=>{
+                return ['Romantic Cooking', 'Sailing', 'All-Night Coding']
+            },
+            createEvent:(args)=>{
+                 const eventName = args.name;
+                 return eventName;
+            }
+        },
+        graphiql: true
+    })
+);
 
 
 app.listen(3000);
